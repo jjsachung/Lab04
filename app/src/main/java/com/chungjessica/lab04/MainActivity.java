40254fac04ef6ca36ac738c4c37293c5e103cfa6
jssachung <jessica.s.chung101@gmail.com>
2021-09-20 15:54:50 -0400

    added overlapping layout and scrollview with textview

--- app/src/main/java/com/chungjessica/lab04/MainActivity.java
+++ app/src/main/java/com/chungjessica/lab04/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.backgroundcolor);
+        setContentView(R.layout.scrollview_with_textview);
     }
 
     public void changeLayout(View view) {
