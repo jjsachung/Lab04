11f7be29625b13d2dba67a38ed3105097ab1ae9f
jssachung <jessica.s.chung101@gmail.com>
2021-09-22 14:05:34 -0400

    added horizontal scrollview and constraint layout

--- app/src/main/java/com/chungjessica/lab04/MainActivity.java
+++ app/src/main/java/com/chungjessica/lab04/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.scrollview_with_textview);
+        setContentView(R.layout.constraint_layout);
     }
 
     public void changeLayout(View view) {
